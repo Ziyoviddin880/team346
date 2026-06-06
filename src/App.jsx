3b7ed3b959da8141664fedda2c415a69660fa553
Ozodbek <>
2026-06-06 14:04:49 +0500

    Add Ozodbek

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+// import React from "react";
 
 function App() {
   return (
     <div>
       <h1>346 team start project</h1>
+      <h3>Ozodbek Branch</h3>
     </div>
   );
 }
